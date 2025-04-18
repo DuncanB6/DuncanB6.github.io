ef6b4ba34dfde0ad6e16359c9aa7230ff032ec77
DUNCAN BOYD <duncan@wapta.ca>
2025-04-18 09:50:30 -0700

    followed youtube tutorial to build a demo site

---
+++ app.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+});
+
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true);
+document.querySelector(".rotating-logos").appendChild(logos);
+
+window.addEventListener("load", () => {
+    const yearElement = document.getElementById("year");
+    const currentYear = new Date().getFullYear();
+    yearElement.textContent = currentYear;
+})

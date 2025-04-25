3caf0fb722d37b8ce98ce886332ac7a7ba2b1690
DUNCAN BOYD <duncan@wapta.ca>
2025-04-25 12:06:46 -0600

    added links to buttons

--- app.js
+++ app.js
@@ -10,4 +10,28 @@ window.addEventListener("load", () => {
     const yearElement = document.getElementById("year");
     const currentYear = new Date().getFullYear();
     yearElement.textContent = currentYear;
-})
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const aboutBtn = document.getElementById("aboutBtn");
+    const expBtn = document.getElementById("expBtn");
+    const projBtn = document.getElementById("projBtn");
+  
+    if (aboutBtn) {
+      aboutBtn.addEventListener("click", () => {
+        window.location.href = "about.html";
+      });
+    }
+  
+    if (expBtn) {
+      expBtn.addEventListener("click", () => {
+        window.location.href = "experiences.html";
+      });
+    }
+
+    if (projBtn) {
+        projBtn.addEventListener("click", () => {
+          window.location.href = "projects.html";
+        });
+      }
+  });
